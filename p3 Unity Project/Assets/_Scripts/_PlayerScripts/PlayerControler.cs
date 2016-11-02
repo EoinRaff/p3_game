fbec145ce2f2917a0c00387be998db98024fb5f5
Sune <enxbox@hotmail.com>
2016-11-02 15:35:53 +0100

    Fuck dig Eoin

---
+++ p3 Unity Project/Assets/_Scripts/_PlayerScripts/PlayerControler.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+// Thise will automatically add the mentioned component when the program starts. In this pertically case it is a component called PlayerMotor. 
+// This will in turn invoke that a Rigidbody body is added (Look at script PlayerMotor line 3)
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerControler : MonoBehaviour {
+
+	[SerializeField] 
+	float speed,mouseSensetivity;
+	
+	
+
+	private PlayerMotor motor; 
+
+	void Start () {
+
+		motor = GetComponent <PlayerMotor> (); 
+
+	}
+
+	void Update () {
+		
+		// When you use GetAxisRaw, you get the data without unity making any smoothing that you would get if you used GetAxis. 
+		// When using Horizontal and Vertical axis, the value is going to between the range of -1 to 1 (for them both individually)
+		float xMovement = Input.GetAxisRaw("Horizontal");
+		float zMovement = Input.GetAxisRaw ("Vertical");
+
+		// Like the previus floates we acess some input information, this time it would be from the mouse 
+		float yRotation = Input.GetAxisRaw ("Mouse X");
+		float xRotation = Input.GetAxisRaw ("Mouse Y");
+
+		//Debug.Log (xRotation);
+
+
+		// This creates a 3D vector that makes the object able to move. The reason for using transform is that this takes into consideration the current location of the object.
+		Vector3 getMovingHorizontal = transform.right * xMovement;
+		Vector3 getMovingVertical = transform.forward * zMovement;
+
+		//Here to add the previous vectors together, and normalize (this mean that the total value have to be one) 
+		Vector3 velocity = (getMovingHorizontal + getMovingVertical).normalized * speed;
+
+		//Here a vector is created which will used to create the rotation. Here the mouse sensativity is an factor which influence how much the rotation is affeceted. 
+		Vector3 rotation = new Vector3 (0,yRotation,0) * mouseSensetivity;
+		Vector3 camRotation = new Vector3 (xRotation ,0,0) * mouseSensetivity;
+
+
+		//Here a function inside PlayerMotor is called 
+		motor.move (velocity);
+		motor.getRotation (rotation);
+		motor.getCamRotation (camRotation);
+	}
+
+
+
+
+}
---
+++ p3 Unity Project/Assets/_Scripts/_PlayerScripts/PlayerMotor.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+// Thise will automatically add the mentioned component when the program starts. 
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerMotor : MonoBehaviour {
+
+	// This makes the variable visible inside Unity eventhough it´s declared as private. 
+	[SerializeField]
+	Camera playerCam;
+
+	Vector3 velocity; 
+	Vector3 rotation; 
+	Vector3 camRotation;
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent <Rigidbody> (); 
+	}
+	
+	//Insted of runnning each frame (the the function Update will do), we insted run each frame there is a physical change 
+	void FixedUpdate () {
+		getTheObjectMoving ();
+		getTheObjectRotating ();
+
+		Debug.Log (camRotation);
+
+	}
+
+
+	//Here we get the the velocity from the playerControle scripts 
+	public void move (Vector3 _velocity) {
+
+		velocity = _velocity;
+	}
+
+	//Here we get the the rotation from the playerControle scripts 
+	public void getRotation (Vector3 _rotation) {
+
+		rotation = _rotation;
+	}
+
+	public void getCamRotation (Vector3 _camRotation) {
+		camRotation = _camRotation; 
+	}
+
+	void getTheObjectMoving () {
+
+		//Insures that there has happen some change to the our velocity vector
+		if (velocity != Vector3.zero) {
+
+		// Here we call the method: Moveposition, this has the ability to check whether our object is colliding with any objects inside the created world
+			// The time.fixedDeltaTime, is the is the seconds of the interval of the object changes 
+
+			rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime); 
+		}
+	}
+
+	// This method applies the rotation to the Rigidbody
+	 void getTheObjectRotating () {
+
+		// Here we call the method MoveRotation which are contained inside the Rigidbody class (this is the same for rb.rotation). 
+		// The Quaternion is a way of representing rotation, and by calling Quaternion.Euler we give it the input of the axis zyx.
+		rb.MoveRotation (rb.rotation * Quaternion.Euler (rotation));
+		playerCam.transform.Rotate (-camRotation);
+
+	}
+
+
+
+}
