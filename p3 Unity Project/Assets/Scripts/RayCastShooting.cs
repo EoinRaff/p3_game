922158364e3926ce1ff5b898a37f85faaf4b15fa
Eoin_Rafferty <eoinrafferty@gmail.com>
2016-11-08 16:12:23 +0100

    removed particle effects, added lights

--- p3 Unity Project/Assets/Scripts/RayCastShooting.cs
+++ p3 Unity Project/Assets/Scripts/RayCastShooting.cs
@@ -12,7 +12,6 @@ public class RayCastShooting : MonoBehaviour
     public float weaponRange = 50f;                                     // Distance in Unity units over which the player can fire
     public float hitForce = 100f;                                       // Amount of force which will be added to objects with a rigidbody shot by the player
     public Transform gunEnd;                                            // Holds a reference to the gun end object, marking the muzzle location of the gun
-    public ParticleSystem partical;
 
     private Camera fpsCam;                                              // Holds a reference to the first person camera
     private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);    // WaitForSeconds object used by our ShotEffect coroutine, determines time laser line will remain visible
@@ -116,7 +115,6 @@ public class RayCastShooting : MonoBehaviour
     {
         // Turn on our line renderer
         laserLine.enabled = true;
-        partical.Play();
 
         //Wait for .07 seconds
         yield return shotDuration;
